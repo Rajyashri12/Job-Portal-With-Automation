4b4f897663c079b2ddb3268b7bae9a17bf02abde
Bhagyashree Yadav <bhagyashreey2020@gmail.com>
2025-06-03 18:05:53 +0530

    files added

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <h1>Find your Dream Job Today</h1>
+      <h1>Connecting Talent with Opportunities Across the Nation For Every Skill Level</h1>
+      <div className='box'>
+      Explore a vast array of job listings in diverse industries. Whether
+      you're a seasoned professional or just starting out, find the perfect
+      role to advance your career. Our platform makes job searching easy and
+      efficient, bringing you closer to your next big opportunity.
+      </div>
+    </section>
+  )
+}
+
+export default Hero
